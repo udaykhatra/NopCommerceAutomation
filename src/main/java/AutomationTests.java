68ca26f5777fef463b9adad43c1cddb64aea61f7
wellbha <wellbha@gmail.com>
2018-12-24 16:48:59 +0000

    with manual test case steps V2.0

--- src/main/java/AutomationTests.java
+++ src/main/java/AutomationTests.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class AutomationTests {
+    //common methods used during the program
     static WebDriver driver;
     public void clickElementBy(By by){
         driver.findElement(by).click();
@@ -25,193 +26,290 @@ public class AutomationTests {
     public void selectBy(By by){
         driver.findElement(by).isSelected();
     }
-    @BeforeMethod
+    @BeforeMethod //run before every method
     public void openingBrowser(){
+        //initializing the chrome driver and passing the url
+        //pre conditions
         System.setProperty("webdriver.chrome.driver","src\\BrowserDriver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().fullscreen();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://demo.nopcommerce.com/");
     }
-    @AfterMethod
+    @AfterMethod //run after every method
     public void closingBrowser(){
         driver.quit();
     }
 
-    @Test(priority=1)
+    @Test(priority=1)   // first test to run to check user should be able to register successfully
     public void userShouldBeAbleToRegisterSuccessfully(){
+        //click on register button
         clickElementBy(By.linkText("Register"));
+        //select the male gender option
         clickElementBy(By.id("gender-male"));
+        //enter "MyFirstName" in First name field
         enterText(By.id("FirstName"),"MyFirstName");
+        //enter "MySurname" in Last name field
         enterText(By.id("LastName"),"MySurname");
+        //select the value "20" for Day field of Date of Birth
         Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
         day.selectByValue("20");
+        //select the value "May" for the Month field of Date of Birth
         Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
         month.selectByIndex(5);
+        //select the value "1950" for the year field of Date of Birth
         Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
         year.selectByValue("1950");
         DateFormat dateFormat = new SimpleDateFormat("DDMMYYYHHMMSS");
         Date date = new Date();
         String date1 = dateFormat.format(date);
+        //enter the value "Bhavin@home.com" in email field
         driver.findElement(By.id("Email")).sendKeys("Bhavin+"+date1+"@home.com");
+        //enter the value "My Company" in the company field
         enterText(By.id("Company"),"My Company");
+        //tick the Newsletter check box
         selectBy(By.id("Newsletter"));
+        //enter the value "mypass" in the Password field
         enterText(By.id("Password"),"mypass");
+        //enter the value "mypass" in the Confirm Password field
         enterText(By.id("ConfirmPassword"),"mypass");
+        //click on register button
         clickElementBy(By.id("register-button"));
         String actualResult = driver.findElement(By.xpath("//div[@class='result']")).getText();
+        //user should be able to see the message "Your registration completed" if successful
         String expectedResult = "Your registration completed";
         Assert.assertEquals("Test Fail",expectedResult,actualResult);
+        //click on Log out button
         findElementBy(By.linkText("Log out"));
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2) //registered user should be able to send email with product successfully
     public void registeredUserShouldBeAbleToSendEmailWithProductSuccessfully(){
+        //click on register button
         clickElementBy(By.linkText("Register"));
+        //select the male gender option
         clickElementBy(By.id("gender-male"));
+        //enter "MyFirstName" in First name field
         enterText(By.id("FirstName"),"MyFirstName");
+        //enter "MySurname" in Last name field
         enterText(By.id("LastName"),"MySurname");
+        //select the value "20" for Day field of Date of Birth
         Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
         day.selectByValue("20");
+        //select the value "May" for the Month field of Date of Birth
         Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
         month.selectByIndex(5);
+        //select the value "1950" for the year field of Date of Birth
         Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
         year.selectByValue("1950");
         DateFormat dateFormat = new SimpleDateFormat("DDMMYYYHHMMSS");
         Date date = new Date();
         String date1 = dateFormat.format(date);
+        //enter the value "Bhavin@home.com" in email field
         driver.findElement(By.id("Email")).sendKeys("Bhavin+"+date1+"@home.com");
+        //enter the value "My Company" in the company field
         enterText(By.id("Company"),"My Company");
+        //tick the Newsletter check box
         selectBy(By.id("Newsletter"));
+        //enter the value "mypass" in the Password field
         enterText(By.id("Password"),"mypass");
+        //enter the value "mypass" in the Confirm Password field
         enterText(By.id("ConfirmPassword"),"mypass");
+        //click on register button
         clickElementBy(By.id("register-button"));
         WebDriverWait wait = new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Computers")));
+        //click on Computers
         clickElementBy(By.linkText("Computers"));
+        //click on Notebooks
         clickElementBy(By.linkText("Notebooks"));
+        //click on the first product in the list
         clickElementBy(By.xpath("//div[@data-productid='4']/div[@class='picture']"));
+        //click on the Email a Friend button
         clickElementBy(By.xpath("//input[@value='Email a friend']"));
+        //enter the value "rakesh@patel.com" in Friend's Email field
         enterText(By.id("FriendEmail"),"rakesh@patel.com");
-        //enterText(By.id("YourEmailAddress"),"ramesh@patel.com");
+        //registered email address should be entered in Your email address field
+        //Enter the value "Best one for you!!!! in the Personal message field
         enterText(By.id("PersonalMessage"),"Best one for you!!!");
+        //click on send email button
         clickElementBy(By.name("send-email"));
+        //registered user should be able to see the message "Your message has bee sent."
         String expectedResult = "Your message has been sent.";
         String actualResult = driver.findElement(By.xpath("//div[@class='page email-a-friend-page']/div[2]/div[contains(text(),'Your message has been sent.')]")).getText();
         Assert.assertEquals("Test case : Test Fail",expectedResult,actualResult);
     }
 
-    @Test (priority = 3)
+    @Test (priority = 3) // unregistered user should not be able to send email with product
     public void unRegisteredUserShouldNotBeAbleToSendEmail(){
+        //click on Computers
         clickElementBy(By.linkText("Computers"));
-        //now clicking on notebooks
+        //click on Notebooks
         clickElementBy(By.linkText("Notebooks"));
-        //Now selecting the product
+        //click on the first product in the list
         clickElementBy(By.xpath("//div[@data-productid='4']/div[@class='picture']"));
-        //clicking on email a friend button
+        //click on the Email a Friend button
         clickElementBy(By.xpath("//input[@value='Email a friend']"));
-        //passing the values in the required fields
+        //enter the value "abc@abc.com" in Friend's Email field
         enterText(By.id("FriendEmail"),"abc@abc.com");
+        //enter value "def@def.com" in Your Email Address field
         enterText(By.id("YourEmailAddress"),"def@def.com");
-        enterText(By.id("PersonalMessage"),"Best one for you");
+        //Enter the value "Best one for you!!!! in the Personal message field
+        enterText(By.id("PersonalMessage"),"Best one for you!!!!");
         //click on send email button
         clickElementBy(By.name("send-email"));
-        //comparing the actual message to expected message to user
+        //"Only registered customers can use email a friend feature" unsuccessful message should be display
         String expectedResult = "Only registered customers can use email a friend feature";
         String actualResult = driver.findElement(By.xpath("//div[@class=\"message-error validation-summary-errors\"]/ul/li")).getText();
         Assert.assertEquals("Test case : Test Fail",expectedResult,actualResult);
     }
 
-    @Test (priority = 4)
+    @Test (priority = 4) //user need to accept the T&C before checkout pop should be displayed
     public void userNeedToAcceptTermsAndConditionBeforeCheckOut(){
+        //click on register button
         clickElementBy(By.linkText("Register"));
+        //select the male gender option
         clickElementBy(By.id("gender-male"));
+        //enter "MyFirstName" in First name field
         enterText(By.id("FirstName"),"MyFirstName");
+        //enter "MySurname" in Last name field
         enterText(By.id("LastName"),"MySurname");
+        //select the value "20" for Day field of Date of Birth
         Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
         day.selectByValue("20");
+        //select the value "May" for the Month field of Date of Birth
         Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
         month.selectByIndex(5);
+        //select the value "1950" for the year field of Date of Birth
         Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
         year.selectByValue("1950");
         DateFormat dateFormat = new SimpleDateFormat("DDMMYYYHHMMSS");
         Date date = new Date();
         String date1 = dateFormat.format(date);
+        //enter the value "Bhavin@home.com" in email field
         driver.findElement(By.id("Email")).sendKeys("Bhavin+"+date1+"@home.com");
+        //enter the value "My Company" in the company field
         enterText(By.id("Company"),"My Company");
+        //tick the Newsletter check box
         selectBy(By.id("Newsletter"));
+        //enter the value "mypass" in the Password field
         enterText(By.id("Password"),"mypass");
+        //enter the value "mypass" in the Confirm Password field
         enterText(By.id("ConfirmPassword"),"mypass");
+        //click on register button
         clickElementBy(By.id("register-button"));
         WebDriverWait wait = new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Computers")));
+        //click on Computers
         clickElementBy(By.linkText("Computers"));
+        //click on Notebooks
         clickElementBy(By.linkText("Notebooks"));
+        //click on the first product in the list
         clickElementBy(By.xpath("//div[@data-productid='4']/div[@class='picture']"));
+        //click on ADD TO CART button
         clickElementBy(By.id("add-to-cart-button-4"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='bar-notification']/p/a")));
+        //click on shopping cart on the notification bar
         clickElementBy(By.xpath("//div[@id='bar-notification']/p/a"));
+        //click on the CHECKOUT button
         clickElementBy(By.id("checkout"));
+        //Pop up display should be display to accept T&C to user
         String expectedResult = "Please accept the terms of service before the next step.";
         String actualResult = driver.findElement(By.id("terms-of-service-warning-box")).getText();
         Assert.assertEquals("Test case : Test Fail",expectedResult,actualResult);
     }
 
-    @Test (priority = 5)
+    @Test (priority = 5)  // registered user should be able to buy single product successfully
     public void registeredUserShouldBeAbleToBuyAnySingleProductSuccessfully(){
+        //click on register button
         clickElementBy(By.linkText("Register"));
+        //select the male gender option
         clickElementBy(By.id("gender-male"));
+        //enter "MyFirstName" in First name field
         enterText(By.id("FirstName"),"MyFirstName");
+        //enter "MySurname" in Last name field
         enterText(By.id("LastName"),"MySurname");
+        //select the value "20" for Day field of Date of Birth
         Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
         day.selectByValue("20");
+        //select the value "May" for the Month field of Date of Birth
         Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
         month.selectByIndex(5);
+        //select the value "1950" for the year field of Date of Birth
         Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
         year.selectByValue("1950");
         DateFormat dateFormat = new SimpleDateFormat("DDMMYYYHHMMSS");
         Date date = new Date();
         String date1 = dateFormat.format(date);
+        //enter the value "Bhavin@home.com" in email field
         driver.findElement(By.id("Email")).sendKeys("Bhavin+"+date1+"@home.com");
+        //enter the value "My Company" in the company field
         enterText(By.id("Company"),"My Company");
+        //tick the Newsletter check box
         selectBy(By.id("Newsletter"));
+        //enter the value "mypass" in the Password field
         enterText(By.id("Password"),"mypass");
+        //enter the value "mypass" in the Confirm Password field
         enterText(By.id("ConfirmPassword"),"mypass");
+        //click on register button
         clickElementBy(By.id("register-button"));
         WebDriverWait wait = new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Computers")));
+        //click on Computers
         clickElementBy(By.linkText("Computers"));
+        //click on Notebooks
         clickElementBy(By.linkText("Notebooks"));
+        //click on the first product in the list
         clickElementBy(By.xpath("//div[@data-productid='4']/div[@class='picture']"));
+        //click on ADD TO CART button
         clickElementBy(By.id("add-to-cart-button-4"));
+        //click on shopping cart on the notification bar
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='bar-notification']/p/a")));
         clickElementBy(By.xpath("//div[@id='bar-notification']/p/a"));
+        //select the T&C check box
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("termsofservice")));
         clickElementBy(By.id("termsofservice"));
+        //click on CHECK OUT button
         clickElementBy(By.id("checkout"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("BillingNewAddress_CountryId")));
         Select country = new Select(driver.findElement(By.id("BillingNewAddress_CountryId")));
+        //registered user details will be autofilled
+        //select the value "United Kingdom" in the country field
         country.selectByVisibleText("United Kingdom");
+        //enter "Wembley" in the city field
         enterText(By.id("BillingNewAddress_City"),"Wembley");
+        //enter the value "123 First Street" in Address 1 field
         enterText(By.id("BillingNewAddress_Address1"),"123 First Street");
+        //enter the value "HA1 1NA" in the zip postal code field
         enterText(By.id("BillingNewAddress_ZipPostalCode"),"HA1 1NA");
+        //enter the value "07866638787" in the phone number field
         enterText(By.id("BillingNewAddress_PhoneNumber"),"07866638787");
+        //click on continue button
         clickElementBy(By.id("billing-buttons-container"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shipping-method-buttons-container")));
+        //accept default value and click on continue button
         clickElementBy(By.id("shipping-method-buttons-container"));
+        //click on credit card radio button
         clickElementBy(By.id("paymentmethod_1"));
+        //click on continue button
         clickElementBy(By.xpath("//input[@class='button-1 payment-method-next-step-button']"));
-        //Select cardType = new Select(driver.findElement(By.id("CreditCardType")));
-        //cardType.selectByValue("Visa");
+        //enter the value "Ramesh Patel" in the card holder name field
         enterText(By.id("CardholderName"),"Ramesh Patel");
+        //enter the value "4111 1111 1111 1111" in the card number field
         enterText(By.id("CardNumber"),"4111 1111 1111 1111");
+        //select the value "02" in the expiry month field
         Select expiryMonth = new Select(driver.findElement(By.id("ExpireMonth")));
         expiryMonth.selectByValue("2");
+        //select the value "2020" in the expiry year value
         Select expiryYear = new Select(driver.findElement(By.id("ExpireYear")));
         expiryYear.selectByValue("2020");
+        //enter the value "737" in the card code field
         enterText(By.id("CardCode"),"737");
+        //click on continue button
         clickElementBy(By.xpath("//input[@class='button-1 payment-info-next-step-button']"));
+        //click on confirm button
         clickElementBy(By.xpath("//input[@class='button-1 confirm-order-next-step-button']"));
+        //"Your order has been successfully processed!" message should be displayed
         String expectedResult = "Your order has been successfully processed!";
         String actualResult = driver.findElement(By.xpath("//strong[contains(text(),'Your order has been successfully processed!')]")).getText();
         Assert.assertEquals("Test case : Test Fail",expectedResult,actualResult);
@@ -219,12 +317,17 @@ public class AutomationTests {
 
     @Test (priority = 6)
     public void userShouldBeAbleToSortByPriceHighToLow(){
+        //click on Computers
         clickElementBy(By.linkText("Computers"));
+        //click on Notebooks
         clickElementBy(By.linkText("Notebooks"));
+        //select the value "Price: High to Low" from sort by field
         Select sortBy = new Select(driver.findElement(By.id("products-orderby")));
         sortBy.selectByVisibleText("Price: High to Low");
+
         WebDriverWait wait = new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-productid='7']")));
+        //verify the products loaded on the page high to low in price by checking first price and last price
         String firstProductPrice = driver.findElement(By.xpath("//div[@data-productid='4']/div[2]/div[3]/div/span")).getText();
         String lastProductPrice = driver.findElement(By.xpath("//div[@data-productid='7']/div[2]/div[3]/div/span")).getText();
         String firstPrice = firstProductPrice.substring(1).replace(",","");
